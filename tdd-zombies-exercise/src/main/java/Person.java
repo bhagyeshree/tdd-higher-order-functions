887a2695eeb3282b8283ff45daca1981fd4f7de4
PaulDimalante <dimalantep@gmail.com>
2018-12-03 17:06:11 -0500

    created tdd higher order function

---
+++ tdd-zombies-exercise/src/main/java/Person.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+final class Person {
+
+    String firstName;
+    String lastName;
+    int age;
+
+        Person(String firstName, String lastName, int age)  {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+
+
+}
